01f4c4b555afa1020d9ecea492026fae0ab71db5
Ebite Miracle Nonso <33063292+mcnony@users.noreply.github.com>
2023-03-13 16:40:17 +0100

    Create poll.php
    Task
    Write a validation logic for a simple voting system. Validations to check includes: Check if voter is above 18 years Check if voter has PVC Check if voter’s ward is 020 If all checks out, echo “Voter eligible to vote”. If any fails, echo why it failed. Push Code to Github and submit link.

---
+++ poll.php
@@ -0,0 +1,32 @@
+<?php
+
+$voter_age = 18;
+$voter_have_pvc = true;
+$voter_ward = "020";
+// Checkif user's age is number
+if(!is_numeric($voter_age)){
+    echo "Your age must be a number.";
+    exit();
+}
+
+// Check if user is an adult
+if($voter_age<18){
+   echo "You must be above 18 to be eligible to vote." ;
+   exit();
+}
+
+// Check if user has PVC
+if(!$voter_have_pvc){
+    echo "You must have a PVC to vote.";
+    exit();
+}
+
+// Validate user's ward
+if($voter_ward!=="020"){
+    echo "Only those in ward ".$voter_ward." are allowed to vote";
+    exit();
+}
+// Voter makes it here if they were validated successfully without any problems
+echo "Hi buddy, you are eligible to vote. Your information are: Age: ".$voter_age.", PVC status: ".($voter_have_pvc=true?"Valid":"Invalid")." Ward: ".$voter_ward;
+// End
+?>
